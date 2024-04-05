fda1e01ce1dbd9d67f83850624d5dbe6710940ef
tomas <tomashorak@post.cz>
2024-04-05 08:50:36 +0200

    play more with iterators, channeled and not channeld

--- utils/iterators.go
+++ utils/iterators.go
@@ -1,18 +1,24 @@
 package utils
 
+import (
+	"fmt"
+)
+
 type IteratorChannel[T any] chan (Result[T, error])
 
 type IteratorFunc[T any] func(channel IteratorChannel[T])
 
 type Iterator[T any] struct {
 	current T
-	channel IteratorChannel[T]
-	link    func()
+	chiter  IteratorChannel[T]
+	chcmd   chan bool
+	itfunc  func()
 }
 
 func (iterator *Iterator[T]) Next() bool {
 	// fmt.Println("reding")
-	result, ok := <-iterator.channel
+	iterator.chcmd <- true
+	result, ok := <-iterator.chiter
 	// fmt.Println("read", result.Value)
 	if ok && result.Err == nil {
 		iterator.current = result.Value
@@ -24,19 +30,75 @@ func (iterator *Iterator[T]) Current() T {
 	return iterator.current
 }
 
+func (iterator *Iterator[T]) Close() {
+	iterator.chcmd <- false
+	close(iterator.chcmd)
+	close(iterator.chiter)
+}
+
 func NewIterator[T any](iteratorFunc IteratorFunc[T]) *Iterator[T] {
 	var iterator Iterator[T]
-	iterator.channel = make(IteratorChannel[T])
-	iterator.link = func() {
-		defer func() {
-			if r := recover(); r != nil {
-				if r != "send on closed channel" {
-					panic(r)
-				}
+	iterator.chiter = make(IteratorChannel[T])
+	iterator.chcmd = make(chan bool)
+	iterator.itfunc = func() {
+		for {
+			cmd, ok := <-iterator.chcmd
+			if cmd && ok {
+				iteratorFunc(iterator.chiter)
+			} else {
+				break
 			}
-		}()
-		iteratorFunc(iterator.channel)
+		}
 	}
-	go iterator.link()
+	go iterator.itfunc()
+	return &iterator
+}
+
+type Iterator2EofErr struct {
+	message string
+}
+
+func (e Iterator2EofErr) Error() string {
+	return e.message
+}
+
+var IterEOF = Iterator2EofErr{message: "eof"}
+
+type Iterator2Func[T any] func() (T, bool)
+type Iterator2[T any] struct {
+	current   T
+	itfunc    Iterator2Func[T]
+	lastError error
+}
+
+func (iterator *Iterator2[T]) Next() bool {
+	if iterator.lastError != nil {
+		return false
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			//fmt.Println("Recovered from panic:", r)
+			iterator.lastError = fmt.Errorf("panic: %v", r)
+		}
+	}()
+	var ok bool
+	iterator.current, ok = iterator.itfunc()
+	if !ok {
+		iterator.lastError = IterEOF
+	}
+	return ok
+}
+
+func (iterator *Iterator2[T]) Current() T {
+	return iterator.current
+}
+
+func (iterator *Iterator2[T]) Error() error {
+	return iterator.lastError
+}
+
+func NewIterator2[T any](iteratorFunc Iterator2Func[T]) *Iterator2[T] {
+	var iterator Iterator2[T]
+	iterator.itfunc = iteratorFunc
 	return &iterator
 }
--- utils/iterators_test.go
+++ utils/iterators_test.go
@@ -11,14 +11,12 @@ func TestIntIterator(t *testing.T) {
 	var seq int32 = 0
 	var iter = NewIterator[int32](
 		func(channel IteratorChannel[int32]) {
-			for {
-				seq = seq + 1
-				var x Result[int32, error]
-				x.Value = seq
-				channel <- x
-			}
+			seq = seq + 1
+			var x Result[int32, error]
+			x.Value = seq
+			channel <- x
 		})
-
+	defer iter.Close()
 	for i := 1; i < 1000000; i++ {
 		if !iter.Next() {
 			break
@@ -28,7 +26,42 @@ func TestIntIterator(t *testing.T) {
 			t.Fatalf("expected %d but got %d", i, iter.current)
 		}
 	}
-	close(iter.channel)
 	elapsed := time.Since(start)
-	fmt.Println("Test1 took ", elapsed)
+	t.Log(t.Name(), " took ", elapsed)
+	iter = nil
+}
+
+func TestIntIterator2(t *testing.T) {
+	start := time.Now()
+	var seq int32 = 0
+	var iter = NewIterator2[int32](
+		func() (int32, bool) {
+			seq = seq + 1
+			if seq > 10 {
+				return 0, false
+			} else {
+				return seq, true
+			}
+		})
+	// for i := 1; i < 1000000; i++ {
+	// 	if !iter.Next() {
+	// 		break
+	// 	}
+	// 	// fmt.Println(iter.current)
+	// 	if int32(i) != iter.current {
+	// 		t.Fatalf("expected %d but got %d", i, iter.current)
+	// 	}
+	// }
+
+	for iter.Next() {
+		fmt.Println(iter.current)
+	}
+	error := iter.lastError
+	if error != nil && error != IterEOF {
+		t.Error(error)
+	}
+
+	elapsed := time.Since(start)
+	t.Log(t.Name(), " took ", elapsed)
+	iter = nil
 }
